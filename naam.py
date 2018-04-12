1f2c1479703d82dd6acdd877e31b9a2bcda72eaf
SammyH1994 <sammy_heutz@outlook.com>
2018-04-12 13:26:17 +0200

    iets minder formeel

--- naam.py
+++ naam.py
@@ -4,4 +4,4 @@ NAMES = ['Stephen', 'Daan', 'Kees']
 
 def get_name():
 	return random.choice(NAMES)
-print("Hallo, ik ben {naam}".format(naam=get_name()))
+print("Hey, ik ben {naam}".format(naam=get_name()))
